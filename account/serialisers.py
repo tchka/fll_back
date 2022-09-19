d11b78c992b33495c3a64c7d4db8a3ad8dd6e785
Elena <tchka777@gmail.com>
2022-09-19 18:50:35 +0300

    user rigister with pass

--- account/serialisers.py
+++ account/serialisers.py
@@ -7,7 +7,23 @@ from django.contrib.auth import get_user_model
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = get_user_model()
-        fields = '__all__'
+        fields = [
+            'username',
+            'email',
+            'fio',
+            'role',
+            'password'
+        ]
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user = User(
+            email=validated_data['email'],
+            username=validated_data['username']
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
 
 
 class ProfileSerializer(serializers.ModelSerializer):
